5109846376c93dd17a2a34e5f8060a026338434b
annisafathul <annisafathul18@gmail.com>
2026-03-12 14:40:50 +0700

    memasukkan jumlah data

---
+++ InsertionSort.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int arr[20];
+int n;
+
+void input()
+{
+    while (true)
+    {
+        cout << "Masukan Jumlah Data Pada Array : ";
+        cin >> n;
+
+        if (n <= 20)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\nArray Tidak Boleh Lebih Dari 20\n";
+        }
+    }
